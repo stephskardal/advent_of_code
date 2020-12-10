09651ad2f0d7e0426b9748a813d34b99346093a9
Steph Skardal <steph.skardal@upstart.com>
2020-12-10 14:54:29 -0500

    Cleanup, because.

--- day10.rb
+++ day10.rb
@@ -1,42 +1,18 @@
-def n_choose_k(n, k)
-  # puts "#{n} and #{k}"
-  return 1 if n == k
-  return 0 if k == 0
-  (k + 1 .. n).reduce(:*) / (1 .. n - k).reduce(:*)
-end
-
 def part_1(data)
-  totals = {}
-  (1..data.size - 1).each do |index|
-    puts "index: #{index} size: #{data.size}"
-    diff = (data[index] - data[index - 1]).to_s
-    totals[diff] ||= 0
-    totals[diff] += 1
-  end
-  # something weird with adding one?
-  totals["3"] += 1
-  totals["1"] += 1
-  totals["3"]*totals["1"]
+  combos = data.each_cons(2).to_a.map{ |b| b[1] - b[0] }
+  combos.count(3)*combos.count(1)
 end
 
-# 9256148959232
 def part_2(data)
   required_vals = []
-  (0..data.size - 1).each do |index|
-    if(index != 0 && ((data[index] - data[index - 1]) == 3))
-      required_vals << data[index]
-      required_vals << data[index - 1]
-    end
-    if(index == 0 || index == (data.size - 1))
-      required_vals << data[index]
-    end
-  end
-  required_vals = required_vals.sort.uniq
+  data = data.unshift(0)
+  new_required = [0, data.each_cons(2).to_a.select { |b| b[1] - b[0] == 3 }, data.last].flatten.uniq
   remaining_vals = data - required_vals
 
   counts = []
-  (1..required_vals.size - 1).map do |index|
-    in_between = remaining_vals.select { |b| b < required_vals[index] && b > required_vals[index - 1] }
+  new_required.each_cons(2).to_a.each do |bounds|
+    in_between = remaining_vals.select { |b| b > bounds[0] && b < bounds[1] }
+    puts "#{in_between} #{bounds.inspect}"
 
     next if in_between.empty?
 
@@ -52,55 +28,6 @@ def part_2(data)
     end
   end
   counts.reduce(:*)
-end
-
-def really_short_inputs
-  "16
-10
-15
-5
-1
-11
-7
-19
-6
-12
-4".split("\n").map(&:to_i).sort
-end
-
-def short_inputs
-  "0
-28
-33
-18
-42
-31
-14
-46
-20
-48
-47
-24
-23
-49
-45
-19
-38
-39
-11
-1
-32
-25
-35
-8
-17
-7
-9
-4
-2
-34
-10
-3".split("\n").map(&:to_i).sort
 end
 
 def inputs
@@ -197,6 +124,5 @@ def inputs
 44
 39
 3
-72
-0".split("\n").map(&:to_i).sort
+72".split("\n").map(&:to_i).sort
 end
