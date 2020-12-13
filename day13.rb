0e2f5875328372eb6ce65cc6c4eaa2aa9fe651bc
Steph Skardal <steph.skardal@upstart.com>
2020-12-13 09:12:31 -0500

    Why do I torture myself?

--- day13.rb
+++ day13.rb
@@ -1,23 +1,25 @@
-# 1001796
-# 37-0
-# 41-27
-# 457-37
-# 13-50
-# 17-51
-# 23-60
-# 29-66
-# 431-68
-# 19-87
-
-def part_1(data)
-  # I don't have this anymore because I wrote it pretty fast.
+def part_1(timestamp, data, mods = [])
+  buses = data.split(',').delete_if { |a| a == "x" }.map(&:to_i)
+  found = false
+  og_timestamp = timestamp
+  while(!mods.any? { |b| b == 0 })
+    mods = buses.map { |bus| timestamp % bus }
+    timestamp += 1
+  end
+  "#{buses[mods.index(0)]*(timestamp-og_timestamp - 1)}"
 end
 
 def part_2(data)
   mods = data.map.with_index { |b, index| [b, index] }.delete_if { |a| a[0] == "x" }.map { |b| "(x+#{b[1]}) mod #{b[0]} == 0" }.join(',')
+
+  # Brute force similar to the method above
+  # But, Ruby is not particular fast on this
+
   # And then plug into wolframalpha because I don't do that kind of math
 end
 
-def inputs
-  "37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,457,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,x,x,x,x,23,x,x,x,x,x,29,x,431,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,19".split(",")
+def bus_inputs
+  # 1001796
+
+  "37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,457,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,x,x,x,x,23,x,x,x,x,x,29,x,431,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,19"
 end
