e1987aa38ae371fde31f5af70aaef27d39774523
Steph Skardal <steph.skardal@upstart.com>
2021-12-19 22:44:19 -0500

    Still buggy.

--- 2021/day18.rb
+++ 2021/day18.rb
@@ -32,71 +32,50 @@ def try_reduction(str)
 end
 
 def reduce(str)
-  split_match_index = nil
-  nested_match_index = nil
-
-  split_matches = str.scan(/([0-9][0-9])/)
-  split_match = nil
-  split_matches.each do |match|
-    split = str.split(match[0], 2)
-    if split[0].count('[') >= 4 && split[1].count(']') >= 4
-      split_match_index = split[0].length
-      split_match = [split[0], match[0], split[1]]
-      break
-    end
+  matches = []
+  str.scan(/([0-9][0-9]|\[{1}[0-9],[0-9]\]{1})/) do |c|
+    matches << [c, $~.offset(0)[0]]
   end
 
-  nested_matches = []
-  str.scan(/(\[[0-9],[0-9]\])/) do |c|
-    nested_matches << [c, $~.offset(0)[0]]
-  end
-
-  nested_match = nil
-  nested_matches.each do |match, length|
-    arr = str.split('')
-    z = arr.shift(length).join('')
-    r = arr.pop(str.length - length - 5).join('')
-    split = [z, r]
-    if (split[0].count('[') - split[0].count(']')) >= 4 && split[1].count(']') >= 4
-      nested_match_index = split[0].length
-      nested_match = [split[0], match[0].gsub(/\[/, '').gsub(/\]/, ''), split[1]]
-      break
-    end
-  end
-
+  first_match = nil
   type = nil
-
-  # get priority left
-  if !split_match.nil? && !nested_match.nil?
-    type = 'split'
-    type = 'nest' if nested_match_index <= split_match_index
-  elsif !split_match.nil?
-    type = 'split'
-  elsif !nested_match.nil?
-    type = 'nest'
+  matches.each do |match, length|
+    arr = str.split('')
+    left_side = arr.shift(length).join('')
+    match_length = match[0].split('').length
+    right_side = arr.pop(str.length - length - match_length).join('')
+    if match_length == 5 && (left_side.count('[') - left_side.count(']')) >= 4
+      first_match = [left_side, match[0].gsub(/\[/, '').gsub(/\]/, ''), right_side]
+      type = 'nest'
+      break
+    end
+    if match_length == 2
+      type = 'split'
+      first_match = [left_side, match[0], right_side]
+    end
   end
 
   return str if type.nil?
 
   if type == 'split'
-    left = split_match[1].to_i/2.floor
-    right = split_match[1].to_i/2.floor + (split_match[1].to_i%2 == 1 ? 1 : 0)
+    left = first_match[1].to_i/2.floor
+    right = first_match[1].to_i/2.floor + (first_match[1].to_i%2 == 1 ? 1 : 0)
     divided = "[#{left},#{right}]"
-    [split_match[0], divided, split_match[2]].join('')
+    [first_match[0], divided, first_match[2]].join('')
   elsif type == 'nest'
-    shift_left, shift_right = nested_match[1].gsub(/\]/, '').gsub(/\[/, '').split(',').map(&:to_i)
-    left_matches = nested_match[0].scan(/(.*)([0-9])(.*)/).last
+    shift_left, shift_right = first_match[1].gsub(/\]/, '').gsub(/\[/, '').split(',').map(&:to_i)
+    left_matches = first_match[0].scan(/(.*)([0-9])(.*)/).last
     if !left_matches.nil?
       insert_left = [left_matches[0], (left_matches[1].to_i + shift_left), left_matches[2]].join('')
     else
-      insert_left = nested_match[0]
+      insert_left = first_match[0]
     end
 
-    right_matches = nested_match[2].scan(/(.*?)([0-9])(.*)/).first
+    right_matches = first_match[2].scan(/(.*?)([0-9])(.*)/).first
     if !right_matches.nil?
       insert_right = [right_matches[0], (right_matches[1].to_i + shift_right), right_matches[2]].join('')
     else
-      insert_right = nested_match[2]
+      insert_right = first_match[2]
     end
 
     [insert_left, "0", insert_right].join('')
@@ -105,14 +84,11 @@ end
 
 str = ["[[[[[9,8],1],2],3],4]", "[7,[6,[5,[4,[3,2]]]]]", "[[6,[5,[4,[3,2]]]],1]", "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"]
 str = ["[[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]],[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]]"]
-#str = ["[[[[4,0],[5,4]],[[7,7],[6,0]]],[[[6,6],[0,5]],[[5,6],[6,[[7,7],8]]]]]"]
-#str = ["[[[[4,0],[5,4]],[[7,7],[6,0]]],[[[6,6],[0,[5,5]]],[[0,6],[6,[7,15]]]]]"]
-#str = ["[[[[[6,6],[7,7]],[[0,7],[7,7]]],[[[5,5],[5,6]],9]],[1,[[[9,3],9],[[9,0],[0,7]]]]"]
 
 str.each do |inside_str|
   result = try_reduction(inside_str)
-  "inside_str: #{inside_str} -> #{result}"
-  p magnitude(eval(result))
+  p "inside_str: #{inside_str} -> #{result}"
+  #p magnitude(eval(result))
 end
 
 =begin
