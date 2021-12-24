c51da7f6b16de84894bb89dd6d9f4202855dd817
Steph Skardal <steph.skardal@upstart.com>
2021-12-24 14:41:52 -0500

    Day 24.

--- 2021/day24.rb
+++ 2021/day24.rb
@@ -36,20 +36,59 @@ data.each do |line|
 end
 maths = maths.join(';').split('w=9;')
 maths.shift
+@maths = maths.map { |b| b.split(';') }
+
+# Dedupping across the maths
+non_matching = []
+diffs = []
+@maths[0].each_with_index do |m, index|
+  non_matching << index if !@maths.all? { |inner_math| inner_math[index] == m }
+end
+@maths.each do |m|
+  diffs << non_matching.map { |nm_i| m[nm_i].match(/[-0-9]+/).to_s.to_i }
+end
+
+# And, you lost me
+maxes = Array.new(14, 0)
+mins = Array.new(14, 0)
+q = []
+
+@maths.each_with_index do |maths, a|
+  if diffs[a][0] == 1
+    q.push([a, diffs[a][2]])
+  else
+    b, y = q.pop
+    delta = y + diffs[a][1]
+    if delta < 0
+      a, b, delta = b, a, -delta
+    end
+    maxes[a], maxes[b] = 9, 9 - delta
+    mins[b], mins[a] = 1, 1 + delta
+  end
+end
+
+p maxes.join('')
+p mins.join('')
+
+=begin
+The Brute Force way
+
 @maths = []
 maths.each do |m|
-  m = m.gsub('x=0;x=(x+z);x=(x%26);z=(z/1);', 'x=z%26;').gsub('y=0;y=(y+25);y=(y*x);', 'y=25*x;')
-  m = m.gsub('x=0;x=(x+z);x=(x%26);z=(z/26);', 'x=z%26;z=(z/26);')
-  m = m.gsub('y=0;y=(y+w);', 'y=w;').gsub('y=25*x;y=(y+1);', 'y=25*x+1;')
-  m = m.gsub(/y=w;y=\(y\+([0-9])\);/) { |z| "y=(w+#{$1});" }
-  m = m.gsub('y=w;y=(y+5);y=(y*x);z=(z+y)', 'y=(w+5)*x;z=z+y;')
-  m = m.gsub(/y=\(w\+([0-9]+)\);y=\(y\*x\);z=\(z\+y\)/) { |z| "y=(w+#{$1})*x;z=(z+(w+#{$1})*x)" }
-  m = m.gsub('x=((x==w)?1:0);x=((x==0)?1:0);', 'x=((x==w)?0:1);')
-  m = m.gsub(/x=z%26;x=\(x\+([-0-9]+)\);x=\(\(x==w\)\?0:1\);/) { |z| "x=(((z%26+#{$1})==w)?0:1);" }
+  #m = m.gsub('x=0;x=(x+z);x=(x%26);z=(z/1);', 'x=z%26;').gsub('y=0;y=(y+25);y=(y*x);', 'y=25*x;')
+  #m = m.gsub('x=0;x=(x+z);x=(x%26);z=(z/26);', 'x=z%26;z=(z/26);')
+  #m = m.gsub('y=0;y=(y+w);', 'y=w;').gsub('y=25*x;y=(y+1);', 'y=25*x+1;')
+  #m = m.gsub(/y=w;y=\(y\+([0-9])\);/) { |z| "y=(w+#{$1});" }
+  #m = m.gsub('y=w;y=(y+5);y=(y*x);z=(z+y)', 'y=(w+5)*x;z=z+y;')
+  #m = m.gsub(/y=\(w\+([0-9]+)\);y=\(y\*x\);z=\(z\+y\)/) { |z| "y=(w+#{$1})*x;z=(z+(w+#{$1})*x)" }
+  #m = m.gsub('x=((x==w)?1:0);x=((x==0)?1:0);', 'x=((x==w)?0:1);')
+  #m = m.gsub(/x=z%26;x=\(x\+([-0-9]+)\);x=\(\(x==w\)\?0:1\);/) { |z| "x=(((z%26+#{$1})==w)?0:1);" }
   @maths << m
 end
 
 yolo = 49917929935111
+yolo = 49999999999999
+
 while(true) do
   if !yolo.to_s.match(/0/)
     inputs = yolo.to_s.split('')
@@ -66,3 +105,4 @@ while(true) do
   end
   yolo = yolo - 1
 end
+=end
